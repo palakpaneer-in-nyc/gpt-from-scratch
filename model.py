3ba016bf0f1048853490ad856128288f1eb3d409
palakpaneer-in-nyc <payal.pri0994@gmail.com>
2026-07-12 11:47:12 -0400

    Stage6: full GPT model

--- model.py
+++ model.py
@@ -72,6 +72,12 @@ class FFN(nn.Module):
     def __init__(self, n_embed, dropout=0.0):
         super().__init__()
         self.net = nn.Sequential(
+            # ***** nn.Linear(in, out, bias=True) ************
+            # linear = nn.Linear(in, out);  learned weight matrix [out, in]
+            # linear(x) applied as x @ weight.T (i.e. only transforms last dimension)
+            # x.shape = [B, T, n_embed]; nn.Linear(n_embed, 4 * n_embed)
+            # linear(x) = [B, T, n_emebd] @ [n_embed, 4 * embed] -> [B, T, 4 * embed]
+            # ****** x @ linear.weight.T + linear.bias ********
             nn.Linear(n_embed, 4 * n_embed), # expand to 4x width -> why?
             nn.GELU(), # activation (smooth, non-zero for negatives -> let the negatives pass through)
             nn.Linear(4 * n_embed, n_embed), # project back down -> what's happening??
@@ -85,7 +91,9 @@ class FFN(nn.Module):
 class Block(nn.Module):
     """
     Full transformer block.
-    Pre-LayerNorm architecture: LayerNorm applied before attention and FFN.
+    Pre-LayerNorm architecture: LayerNorm applied before attention and FFN 
+    # LayerNorm is applied to avoid drifts, exponential bloats in activations 
+    # (values in embed vectors) as they go through several stages/layers of mult+add.
     Residual connections after both sub-layers.
 
     Takes:      x of shape [B, T, n_embed]
@@ -101,8 +109,134 @@ class Block(nn.Module):
 
     def forward(self, x):
         # Pre-LN + residual connection around attention
+        # Learns relationships/connection weights between the tokens.
         x = x + self.attn(self.ln1(x))
 
         # Pre-LN + residual connection around FFN
+        # Learns the deeper knowledge per token independently.
         x = x + self.ffn(self.ln2(x))
         return x
+    
+
+class GPT(nn.Module):
+    """
+    Full GPT language model.
+
+    Takes: ids of range [B, T] - token ids of batch size B and sequence length T
+    Returns: logits [B, T, vocab_size] and optionally loss (perhaps scoring all token in vocab as next token)
+    """
+
+    def __init__(self, vocab_size, block_size, n_embed, n_heads, n_layers, dropout=0.0):
+        super().__init__()
+        self.block_size = block_size
+
+        self.transformer = nn.ModuleDict({
+            'embedding' : GPTEmbedding(vocab_size, block_size, n_embed, dropout),
+            'blocks'    : nn.Sequential(*[
+                Block(n_embed, n_heads, block_size, dropout)
+                for _ in range(n_layers)
+            ]),
+            'ln_f'      : nn.LayerNorm(n_embed)
+        })
+
+        # Output head - projects vector [n_embed] -> score [vocab_size]
+        # [n_embed] @ [n_embed, vocab_size] -> [vocab_size]
+        # 1 64-dim vector -> 65 scores, one per token
+        self.lm_head = nn.Linear(n_embed, vocab_size, bias=False)
+
+        # Weights tying - share embedding and lm_head weights
+        # * Token embedding weight matrix [vocab_size, n_embed] and
+        # * lm_head matrix [vocab_size, n_embed] are the same object (physical matrix) in memory
+        # * Save parameters, halves the memory (if not) and empirically improves the performance
+        # * conceptually, both are essentially answering "what does token 'c' look like in n_embed
+        # dimensional space.
+        self.transformer['embedding'].token_embed.weight = \
+            self.lm_head.weight
+        
+        # Intitalize weights (recursively  called for all sduless/sub-modules); 
+        # if not initialized properly the training will crash.
+        self.apply(self._init_weights) # ???
+
+        print(f"GPT initialized - {self.count_params()/1e6:2f}M parameters")
+
+    def _init_weights(self, module):
+        """
+        Initialize weights following GPT-2 paper.
+        Linear and Embeddings layers: normal distribution std:0.02
+        Bias: zero initialized
+        """
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        elif isinstance(module, nn.LayerNorm):
+            nn.init.ones_(module.weight)
+            nn.init.zeros_(module.bias)
+    
+    def count_params(self):
+        return sum(p.numel() for p in self.parameters())
+    
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        assert T <= self.block_size, \
+            f"Sequence length {T} exceeds block_size {self.block_size}"
+        
+        # Embedding
+        x = self.transformer['embedding'](idx)  # [B, T, n_embed]
+
+        # N transformer blocks
+        x = self.transformer['blocks'](x)       # [B, T, n_embed]
+
+        # Final LayerNorm
+        x = self.transformer['ln_f'](x)         # [B, T, n_embed]
+
+        # Project to vocabulary
+        logits = self.lm_head(x)                # [B, T, vocab_size]
+
+        # Compute loss if targets provided
+        loss = None
+        if targets is not None:
+            B, T, V = logits.shape
+            # CrossEntropyLoss expects [B*T, V] and [B, T]
+            loss = nn.functional.cross_entropy(
+                logits.view(B * T, V),
+                targets.view(B * T)
+            )
+        return logits, loss
+    
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+        """
+        Generate max_new_tokens tokens autoregressively.
+        idx: starting context [B, T]
+        """
+        for _ in range(max_new_tokens):
+            # Crop to block_size if sequence too long
+            idx_cond = idx[:, -self.block_size:]  # LOL - help me understand this?
+
+            # Forward pass - no targets so loss computed
+            logits, _ = self(idx_cond)
+
+            # Take logits at last position - next token prediction
+            logits = logits[:, -1, :]  # [B, vocab_size]
+
+            # Apply temperature - higher = more random, lower = more deterministic
+            logits = logits / temperature
+
+            # optional top-K sampling -- zero all out except Top K logits
+            if top_k is not None:
+                values, _ = torch.topk(logits, top_k)
+                logits[logits < values[:, -1:]] = float('-inf')
+
+            # Softmax to probabilities
+            probs = nn.functional.softmax(logits, dim=-1)
+
+            # Sample next token
+            next_token = torch.multinominal(probs, num_samples=1) # [B, 1]
+
+            # Append to sequence
+            idx = torch.cat([idx, next_token], dim=1)  # [B, T+1]
+        
+        return idx
