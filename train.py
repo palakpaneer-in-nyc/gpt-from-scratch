5def6b15703b06e4005bb586eaee1e675645e908
palakpaneer-in-nyc <payal.pri0994@gmail.com>
2026-07-13 22:07:44 -0400

    Stage 7: fix device

--- train.py
+++ train.py
@@ -35,11 +35,11 @@ config = {
 
 # ------ Device -----------------------------------
 
-device = {
+device = (
     'cuda' if torch.cuda.is_available() else
-    'mps'   if torch.backends.mps.is_available() else
+    'mps'  if torch.backends.mps.is_available() else
     'cpu'
-}
+)
 print(f"using device: {device}")
 
 # ------ Data --------------------------------------
